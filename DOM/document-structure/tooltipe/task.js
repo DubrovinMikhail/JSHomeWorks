b4c34e4fce81857dca6bd9cc54250d204677e4c6
Mikhai Dubrovin <mixailgoo@gmail.com>
2023-02-08 13:22:34 +0300

    task error correction

--- DOM/document-structure/tooltipe/task.js
+++ DOM/document-structure/tooltipe/task.js
@@ -4,14 +4,14 @@ tooltipDiv.className = 'tooltip';
 
 for (let tooltip of tooltips) {
   tooltip.addEventListener('click', (event) => {
-    if (event.target.firstElementChild) {
+    if (event.target.nextSibling.className == 'tooltip tooltip_active') {
       tooltipDiv.classList.toggle('tooltip_active');
     }else {
       tooltipDiv.textContent = tooltip.title;
       tooltipDiv.classList.add('tooltip_active');
-      tooltipDiv.style.marginTop = "0px";
-      tooltipDiv.style.marginLeft = `${event.target.getBoundingClientRect().left}px`;
-      event.target.appendChild(tooltipDiv);
+      tooltipDiv.style.top = `${event.target.getBoundingClientRect().top + 20}px`;
+      tooltipDiv.style.left = `${event.target.getBoundingClientRect().left}px`;
+      event.target.after(tooltipDiv);
     };
     event.preventDefault();
   })
