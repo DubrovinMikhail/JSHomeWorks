c07b2b342a306864ed6b41ba55bdf9d00dee3d35
Mikhai Dubrovin <mixailgoo@gmail.com>
2023-02-14 19:23:52 +0300

    corrected comments

--- Wealth management/client-state/auth/task.js
+++ Wealth management/client-state/auth/task.js
@@ -1,14 +1,17 @@
 const signin = document.getElementById('signin');
 const welcome = document.getElementById('welcome');
 const form = document.forms.signin__form;
-const autorizationStatus = localStorage.getItem('success');
 const userId = document.getElementById('user_id');
 
-if (autorizationStatus === 'true') {
+function hideForm() {
   signin.classList.remove('signin_active');
   welcome.classList.add('welcome_active');
   userId.textContent = localStorage.getItem('userId');
-}
+};
+
+if (localStorage.getItem('userId')) {
+  hideForm();
+};
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -16,18 +19,17 @@ form.addEventListener('submit', (event) => {
   const data = new FormData(event.target);
   const xhr = new XMLHttpRequest;
 
-  xhr.addEventListener('readystatechange', () => {
-    if (xhr.readyState === xhr.DONE) {
-      response = JSON.parse(xhr.responseText);
-      localStorage.setItem('success', response.success);
-      localStorage.setItem('userId', response.user_id);
-      if (!response.success) {
-        alert('неверный логин или пароль');
+  xhr.addEventListener('load', () => {
+    if (xhr.response.user_id) {
+      localStorage.setItem('userId', xhr.response.user_id);
+      hideForm();
+    }else {
+      alert('неверный логин или пароль');
       };
-    };
+    form.reset();
   });
   
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-  
+  xhr.responseType = 'json';
   xhr.send(data);
 });
--- Wealth management/client-state/popup/task.js
+++ Wealth management/client-state/popup/task.js
@@ -1,12 +1,26 @@
 const modal = document.getElementById('subscribe-modal');
 const modalButton = modal.querySelector('.modal__close');
-const modalButtonClick = localStorage.getItem('flag');
+// const modalButtonClick = getCookie('flag');
 
-if (modalButtonClick != 'true') {
+function setCookie(name, value) {
+  document.cookie = name + '=' + encodeURIComponent(value)
+}
+
+function getCookie(name) {
+  if (document.cookie.includes(name)) {
+    const pairs = document.cookie.split('; ');
+    const cookie = pairs.find(p => p.startsWith(name + '='));
+    return cookie.substring(name.length + 1);
+  }
+  return false;
+} 
+
+if (getCookie('flag') != 'true') {
   modal.classList.add('modal_active');
 };
 
 modalButton.addEventListener('click', () => {
-  localStorage.setItem('flag', true);
+  setCookie('flag', 'true');
   modal.classList.remove('modal_active');
+  console.log(document.cookie)
 });
